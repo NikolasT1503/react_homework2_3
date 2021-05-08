5d361153c3ad3e6f84c19fe46f32106781fa5fff
NikolasT1503 <Nikolas_t@mail.ru>
2021-05-08 09:14:58 +0300

    add count n digits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
+
+
 
 function App() {
+  let result = 0;
+  
+  let solve1 = () => {
+    let sum = 0;
+    let num = Number.parseInt(document.getElementById('digit').value);
+    sum = num*(num+1)/2;
+    document.getElementById('res').innerHTML='Результат = '+sum;
+    alert ('Результат = ' + sum);
+    
+    //return sum
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Вычисление суммы n первых натуральных чисел</h2>
+      <TextField
+        id="digit"
+        type="number"
+        label="Введите число n"
+      />
+      <button onClick={solve1}>Рассчитать</button>
+      <h2 id='res'>Результат = </h2>
     </div>
   );
 }
